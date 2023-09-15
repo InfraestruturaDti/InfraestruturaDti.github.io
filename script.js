a5d1b32fab8aa740cd7023b840974447738662ae
Vinicius <vinicius.farias@dtidigital.com.br>
2023-09-15 08:45:55 -0300

    newrackers

--- script.js
+++ script.js
@@ -5,25 +5,15 @@ Utilize apenas imagens locais neste vetor, não links externos, não há suporte
 
 var imagens = [
 
-"imgs/rackers/00.png",
-"imgs/rackers/0.gif",
-"imgs/rackers/foto1rackers.png",
-"imgs/rackers/foto2rackers.png",
-"imgs/rackers/foto3rackers.png",
-"imgs/rackers/foto4rackers.png",
-"imgs/rackers/foto5rackers.png",
-"imgs/rackers/foto6rackers.png",
-"imgs/rackers/foto7rackers.png",
-"imgs/rackers/foto8rackers.png",
-"imgs/rackers/foto9rackers.png",
-"imgs/rackers/28v.gif",
-"imgs/rackers/26v.gif",
-"imgs/rackers/29v.gif",
-"imgs/rackers/25v.gif",
-"imgs/rackers/27v.gif",
-"imgs/rackers/22.jpg",
-"imgs/rackers/23.jpg",
-"imgs/rackers/24.jpg",
+"imgs/rackers/1.png",
+"imgs/rackers/2.png",
+"imgs/rackers/3.png",
+"imgs/rackers/4.png",
+"imgs/rackers/5.png",
+"imgs/rackers/6.png",
+"imgs/rackers/7.png",
+"imgs/rackers/8.png",
+
 
     ]; // array com as imagens
 var tempo = 6000; // tempo em milissegundos para trocar as imagens
