c33a09bf99324170b41fc52b771ab9bec9a57e66
Nilo Can <titolianchen3341@gmail.com>
2024-10-28 10:58:02 -0300

    Adicionando arquivo de bordas

--- ProjetoFotos/Bordas.js
+++ ProjetoFotos/Bordas.js
@@ -2,6 +2,10 @@ export const BORDAS = {
 
     'Inari': 'Bordas/Inari.png',
     'Rackers': 'Bordas/Rackers.png',
+    'Gaia': 'Bordas/Gaia.png',
+    'Aurora': 'Bordas/Aurora.png',
+    'Rubix': 'Bordas/Rubix.png',
+    'Hakuna': 'Bordas/Hakuna.png',
     'default': 'Bordas/Curinga.png'
 
 }
