e2e7707ca4e515c8eca065609d326a3596e67f5e
Nilo Can <titolianchen3341@gmail.com>
2024-10-28 13:20:14 -0300

    Adicionando

--- ProjetoFotos/ScriptPs1/ExportFotos.ps1
+++ ProjetoFotos/ScriptPs1/ExportFotos.ps1
@@ -6,14 +6,20 @@ $photoDirectory = ".\Fotos"
 # Importe os dados do arquivo CSV
 $users = Import-Csv -Path ".\Users.csv"
 $tribos = Import-Csv -Path ".\ListaTribos.csv"
+$blacklist = Import-Csv -Path ".\BlackList.csv"
+
 Get-ChildItem -Path $photoDirectory -File | Remove-Item
 
 
 $i = 0
 foreach ($user in $users) {
+    # Verifique se o usuário está na blacklist
+    if ($blacklist.UserPrincipalName -contains $user.UserPrincipalName) {
+        Write-Host "Skipping user $($user.UserPrincipalName) as they are in the blacklist."
+        continue
+    }
     $i++
     write-host "$i out of $($users.count): $($user.UserPrincipalName)"
-    write-host "At $photoDirectory"
     
     try {
         # Obtenha o ID do usuário pelo endereço de e-mail
@@ -21,6 +27,10 @@ foreach ($user in $users) {
 
         # Nome do arquivo com base na tribo e no e-mail
         $tribo = $tribos | Where-Object { $_.'E-mail' -eq $user.UserPrincipalName } | Select-Object -ExpandProperty Tribo
+        # Se a tribo não for encontrada, defina como "Novato"
+        if (-not $tribo) {
+            $tribo = "Novato"
+        }
         $triboFormatted = $tribo.Split("-")[0].Trim().Replace(" ", "-")
         $nameFormatted = $user.UserPrincipalName.Replace("@dtidigital.com.br", "").Replace(".", "-")
         $NameFile = "$triboFormatted-$nameFormatted"
--- ProjetoFotos/script.js
+++ ProjetoFotos/script.js
@@ -3,10 +3,14 @@ import { BORDAS } from './Bordas.js';
 
 let imagensDisponiveis = [...IMAGENS];
 let imagensExibidas = new Set();
+let posicoesDisponiveis = [];
 
 document.addEventListener('DOMContentLoaded', () => {
     const gridItems = document.querySelectorAll('.grid-item');
     
+    // Inicializa a lista de posições disponíveis
+    posicoesDisponiveis = Array.from(Array(gridItems.length).keys());
+
     gridItems.forEach(item => {
         const randomIndex = Math.floor(Math.random() * imagensDisponiveis.length);
         const imgElement = document.createElement('img');
@@ -15,8 +19,9 @@ document.addEventListener('DOMContentLoaded', () => {
         imgElement.src = imageName;
         imgElement.alt = imageName;
 
-        const triboName = imageName.replace('Fotos/', '').split('-')[0]; // Extrai o nome da tribo
-        const bordaName = BORDAS[triboName] || BORDAS['default']; // Obtém a borda correspondente ou a padrão
+        const parts = imageName.replace('Fotos/', '').split('-');
+        const triboName = parts.length > 0 ? parts[0] : 'default';
+        const bordaName = BORDAS[triboName] || BORDAS['default'];
 
         const borderElement = document.createElement('img');
         borderElement.src = bordaName;
@@ -35,14 +40,22 @@ document.addEventListener('DOMContentLoaded', () => {
             imagensExibidas.clear();
         }
 
-        const randomGridIndex = Math.floor(Math.random() * gridItems.length);
+        if (posicoesDisponiveis.length === 0) {
+            posicoesDisponiveis = Array.from(Array(gridItems.length).keys());
+        }
+
+        const randomPosIndex = Math.floor(Math.random() * posicoesDisponiveis.length);
+        const randomGridIndex = posicoesDisponiveis[randomPosIndex];
+        posicoesDisponiveis.splice(randomPosIndex, 1); // Remove a posição usada
+
         const randomImageIndex = Math.floor(Math.random() * imagensDisponiveis.length);
 
         const selectedItem = gridItems[randomGridIndex];
         const selectedImg = selectedItem.querySelector('img:not(.borda)');
         const newImage = imagensDisponiveis[randomImageIndex];
 
-        const triboName = newImage.split('-')[0];
+        const parts = newImage.replace('Fotos/', '').split('-');
+        const triboName = parts.length > 0 ? parts[0] : 'default';
         const bordaName = BORDAS[triboName] || BORDAS['default'];
         const selectedBorder = selectedItem.querySelector('.borda');
 
