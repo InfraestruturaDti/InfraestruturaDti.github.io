e8f59d67bf4b4dbd2d5a0f3239e09a862a8add28
Nilo Can <titolianchen3341@gmail.com>
2024-11-01 10:03:08 -0300

    Arrumando horario errado

--- ProjetoFotos/Init.ps1
+++ ProjetoFotos/Init.ps1
@@ -5,24 +5,22 @@ try {
     Connect-MgGraph 
     Connect-AzureAD
 
-    Invoke-Expression -Command "git pull"
-    # Listar usuarios ativos
-    Invoke-Expression -Command ".\ScriptPs1\ListarUser.ps1"
-    # Baixar fotos dos usuarios
-    Invoke-Expression -Command ".\ScriptPs1\ExportFotos.ps1"
-    # atualizar variável de fotos
-    Invoke-Expression -Command ".\ScriptPs1\AtualizarVarFotos.ps1"
+    # Invoke-Expression -Command "git pull"
+    # # Listar usuarios ativos
+    # Invoke-Expression -Command ".\ScriptPs1\ListarUser.ps1"
+    # # Baixar fotos dos usuarios
+    # Invoke-Expression -Command ".\ScriptPs1\ExportFotos.ps1"
+    # # atualizar variável de fotos
+    # Invoke-Expression -Command ".\ScriptPs1\AtualizarVarFotos.ps1"
     
-    $horaAlvo = "4:00"  # Defina o horário desejado no formato HH:mm
+    $horaAlvo = "09:53"  # Defina o horário desejado no formato HH:mm
 
 while ($true) {
-    $horaAtual = (Get-Date).ToString("HH:mm")
-    Write-Output "Verificação de horario: $horaAtual"
-    
-    if ($horaAtual -eq $horaAlvo) {
-        # Coloque aqui o código que deseja executar
-        Write-Output "Executando o código às $horaAtual"
-        
+     $horaAtual = (Get-Date).ToString("HH:mm")
+     Write-Output "Verificação de horario: $horaAtual, de $horaAlvo"
+     
+     if ($horaAtual -eq $horaAlvo) {
+        Write-Output "Executando fluxo: $horaAtual"
         # Aguarde um minuto para evitar múltiplas execuções no mesmo minuto
         Start-Sleep -Seconds 60
         Invoke-Expression -Command "git pull"
