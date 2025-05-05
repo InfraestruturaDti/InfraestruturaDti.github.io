a935784ae809a4d9479fcead10e0d78a538a2fa8
Tito Chen <titolianchen3341@gmail.com>
2025-05-05 17:08:53 -0300

    Ataulizando readme e install

--- ProjetoFotos/ScriptPs1/Install.ps1
+++ ProjetoFotos/ScriptPs1/Install.ps1
@@ -1,9 +1,9 @@
+winget install Schniz.fnm
+fnm env --use-on-cd | Out-String | Invoke-Expression
+fnm use --install-if-missing 20
 npm install -g http-server
 npm install -g browser-sync
 Install-Module -Name AzureAD
 Install-Module -Name ExchangeOnlineManagement
 Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
 Install-Module Microsoft.Graph -Scope CurrentUser -Repository PSGallery -Force
-winget install Schniz.fnm
-fnm env --use-on-cd | Out-String | Invoke-Expression
-fnm use --install-if-missing 20
